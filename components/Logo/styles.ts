8ea3c974661baf98fb985f24a1d0c3cd40ac812d
bielsolosos <bielrochasantoscoutinho@gmail.com>
2024-06-24 16:18:31 -0300

    Afirmando aqui bonitinho

--- components/Logo/styles.ts
+++ components/Logo/styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components/native";
 
 export const LimitandoLogo = styled.View`
     width: 125px;
-    height: 125px;
+    height: 130px;
     margin-left: 85px;
     margin-bottom: 10px;
-    margin-top: 10px;
+    margin-top: 5px;
 `
