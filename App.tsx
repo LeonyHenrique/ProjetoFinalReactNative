3276c42ddae6eca8308219cb7df219ce6296ccd8
Amanda Alves <alvesamanda979@gmail.com>
2024-06-25 03:09:08 -0300

    conexão com firebase - início

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {db} from './firebaseConnection';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Routes from './Routes';
 
